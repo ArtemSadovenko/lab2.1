129a195c1e2d9f1977755a24cc1ff03032b04b41
artem sadovenko <asadovenko7@gmail.com>
2021-09-14 21:35:06 +0300

    lab2.1

---
+++ lab2.1_Sadovenko/Source.cpp
@@ -0,0 +1,27 @@
+// Lab_02.cpp
+// Садовенко Артем
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 18
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main(){
+	double a;
+	double z1;
+	double z2;
+	cout << "a = "; cin >> a;
+	if (a == 2 || a == 0 || a == -2 || a == sqrt(2*a)){
+		cout << "not avalable" << endl;
+	}
+	z1 = ((((a + 2) / (sqrt(2 * a))) - (a / (sqrt(2 * a) + 2)) + (2 / (a - sqrt(2 * a)))) * ((sqrt(a) - sqrt(2)) / (a + 2)));
+	//z2 = (1 / (sqrt(a) + sqrt(2)));
+	
+		cout << z1 << endl;
+		//cout << z2 << endl;
+	return 0;
+}
